ffd1e442299a6c3b02d6481a9269428ff568d848
Flakerim Ismani <flakerimi@gmail.com>
2026-04-13 23:09:28 +0200

    release: v1.1.11

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import { generate } from './commands/graph/generate.js'
 import { graphPush } from './commands/graph/push.js'
 import { graphMigrate } from './commands/graph/migrate.js'
 
-export const VERSION = '1.1.10'
+export const VERSION = '1.1.11'
 
 const program = new Command()
 
